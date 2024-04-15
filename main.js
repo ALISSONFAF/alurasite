5cc0d0c7d903df7fccd9cea0f44526c292458da0
OS LARANJAS <107492614+ALISSONFAF@users.noreply.github.com>
2024-04-15 12:26:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T12:10:09");
-const tempoObjetivo2 = new Date("2024-12-05T23:12:12");
-const tempoObjetivo3 = new Date("2024-12-30T14:12:12");
-const tempoObjetivo4 = new Date("2024-12-01T06:08:04");
+const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
